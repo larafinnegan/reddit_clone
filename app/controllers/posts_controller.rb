30132cd4ef091f6783f052d25480f60fc7b7b2ab
Lara Finnegan <lcfinnegan@gmail.com>
2015-04-19 21:15:27 -0400

    add some authorization

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+	before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
+	before_action :correct_user, only: [:edit, :update, :destroy]
+
 	def index
 		@posts = Post.all 
 	end
@@ -23,5 +26,18 @@ class PostsController < ApplicationController
 
 	def post_params
 		params.require(:post).permit(:title, :content, :sub_id, :user_id)
+	end
+
+	def logged_in_user
+		unless logged_in?
+			flash[:danger] = "Please log in."
+			redirect_to login_url
+		end
+	end
+
+	def correct_user
+		@post = Post.find(params[:id])
+		@user = @post.user
+		redirect_to root_url unless current_user?(@user)
 	end
 end
